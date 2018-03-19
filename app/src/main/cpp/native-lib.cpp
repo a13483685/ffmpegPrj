cec15462c72ee11cd4b6d0ed0437d492efe7b2b2
xiezheng <xiez@suchness.cn>
2018-03-19 17:05:50 +0800

    can open 1080p file

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+//#define  LOGW(...) __android_log_print(ANDROID_LOG_WARN,"testff",___VA_ARGS___)
 extern "C" {
 #include <libavcodec/avcodec.h>
 
@@ -15,4 +17,24 @@ Java_demo_test_com_myapplication_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     hello +=avcodec_configuration();
     return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_demo_test_com_myapplication_MainActivity_open(JNIEnv *env, jobject instance, jstring url_,
+                                                   jobject handle) {
+    const char *url = env->GetStringUTFChars(url_, 0);
+
+    // TODO
+    FILE *fp = fopen(url,"rb");
+    if(!fp) {
+//        LOGW("%s open failed !",url_);
+        __android_log_print(ANDROID_LOG_WARN,url,"open failed");
+    }
+    else
+    {
+        __android_log_print(ANDROID_LOG_WARN,url,"open success!");
+        fclose(fp);
+    }
+
+    env->ReleaseStringUTFChars(url_, url);
 }
--- app/src/main/java/demo/test/com/myapplication/MainActivity.java
+++ app/src/main/java/demo/test/com/myapplication/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
+        open("/sdcard/1080.mp4",this);
         tv.setText(stringFromJNI());
     }
 
@@ -26,4 +27,5 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native boolean open(String url,Object handle);
 }
