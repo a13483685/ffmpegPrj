313a52892ea74eba8229058af568c09387cc88bd
xiezheng <xiez@suchness.cn>
2018-03-20 15:19:42 +0800

    增加了视频 音频打印信息

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -9,6 +9,13 @@ extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 }
+using namespace std;
+//判断分母是否为零,防止分母为0 将分数转化为带浮点的小数
+static double r2d(AVRational r)
+{
+    return r.den==0||r.num==0?0:(double)r.num/(double)r.den;
+}
+
 
 extern "C"
 JNIEXPORT jstring
@@ -27,13 +34,61 @@ Java_demo_test_com_myapplication_MainActivity_stringFromJNI(
     AVFormatContext *ic = NULL;
     char path[] = "/sdcard/1080.mp4";
     int ret = avformat_open_input(&ic,path,0,0);
-    if (ret == 0){
-//        __android_log_print(ANDROID_LOG_WARN,"avformat_open_input %s success!",path);
-        LOGW("File %s open sucess!",path);
-    } else {
-//        __android_log_print(ANDROID_LOG_WARN,"avformat_open_input error %s!",av_err2str(ret));
+//    int ret = avformat_find_stream_info(ic,0);
+    if (ret != 0){
         LOGW("File %s open failed",av_err2str(ret));
+//        __android_log_print(ANDROID_LOG_WARN,"avformat_open_input %s success!",path);
+
     }
+    LOGW("File %s open sucess!",path);
+    LOGW("duration = %lld nb_streams = %d",ic->duration,ic->nb_streams );
+//获取流信息
+    ret = avformat_find_stream_info(ic,0);
+    if(ret!=0){
+        LOGW("avformat_find_stream_info failed !");
+    }
+    LOGW("duration = %lld nb_streams = %d",ic->duration,ic->nb_streams );
+
+    /**
+     * 打印视频的信息
+     * nb_streams:里面有的是音频，有的是视频
+     *无法通过下标判断哪个是音频，哪个是视频，需要逐一判断
+     *
+     *
+     */
+
+    int videoStream =0;
+    int fps = 0;
+    int audioStream = 0;
+    for (int i=0;i<ic->nb_streams;i++)
+    {
+        AVStream *as = ic->streams[i];
+        if (as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            LOGW("视频数据");
+            videoStream = i;
+            fps = r2d(as->avg_frame_rate);
+            LOGW("fps = %d,width = %d height =%d codeid = %d pixformat=%d",fps,
+                as->codecpar->width,
+                 as->codecpar->height,
+                 as->codecpar->codec_id,
+                 as->codecpar->format
+            );
+
+        } else if (as->codecpar->codec_type == AVMEDIA_TYPE_AUDIO)
+        {
+            LOGW("音频数据");
+            audioStream = i;
+            LOGW("sample_rate=%d channels=%d sample_format=%d",
+                as->codecpar->sample_rate,
+                as->codecpar->channels,
+                 as->codecpar->format
+            );
+        }
+    }
+
+
+    avformat_close_input(&ic);
     return env->NewStringUTF(hello.c_str());
 
 }
--- app/src/main/java/demo/test/com/myapplication/MainActivity.java
+++ app/src/main/java/demo/test/com/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        open("/sdcard/1080.mp4",this);
+//        open("/sdcard/1080.mp4",this);
         tv.setText(stringFromJNI());
     }
 
