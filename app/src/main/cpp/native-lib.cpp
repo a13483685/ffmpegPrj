7923a519a70a881f8ee3a6b986a8706d57a1e0fe
xiezheng <xiez@suchness.cn>
2018-03-20 09:56:16 +0800

    正常打开文件，在cmakefilelist.txt中加入了avformat，avutil 库编入native-lib

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,13 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
-//#define  LOGW(...) __android_log_print(ANDROID_LOG_WARN,"testff",___VA_ARGS___)
+//___VA_ARGS___
+//#define  LOGW(...) __android_log_print(ANDROID_LOG_WARN,"testff",)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,"testff" ,__VA_ARGS__) // 定义LOGW类型
+
 extern "C" {
 #include <libavcodec/avcodec.h>
-
+#include <libavformat/avformat.h>
 }
 
 extern "C"
@@ -16,7 +19,23 @@ Java_demo_test_com_myapplication_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     hello +=avcodec_configuration();
+    //1.初始化解封装
+    av_register_all();
+    //2.初始化网络
+    avformat_network_init();
+    //3.打开文件
+    AVFormatContext *ic = NULL;
+    char path[] = "/sdcard/1080.mp4";
+    int ret = avformat_open_input(&ic,path,0,0);
+    if (ret == 0){
+//        __android_log_print(ANDROID_LOG_WARN,"avformat_open_input %s success!",path);
+        LOGW("File %s open sucess!",path);
+    } else {
+//        __android_log_print(ANDROID_LOG_WARN,"avformat_open_input error %s!",av_err2str(ret));
+        LOGW("File %s open failed",av_err2str(ret));
+    }
     return env->NewStringUTF(hello.c_str());
+
 }
 extern "C"
 JNIEXPORT jboolean JNICALL
@@ -37,4 +56,5 @@ Java_demo_test_com_myapplication_MainActivity_open(JNIEnv *env, jobject instance
     }
 
     env->ReleaseStringUTFChars(url_, url);
+    return true;
 }
